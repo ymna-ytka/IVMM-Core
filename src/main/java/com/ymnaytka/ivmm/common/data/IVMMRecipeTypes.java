81ff2635744d000b0d92492155ac981a3c779dd7
ymna-ytka <68682618+ymna-ytka@users.noreply.github.com>
2025-11-29 01:04:17 +0200

    Remove IVMMCore mod and all related resources

--- src/main/java/com/ymnaytka/ivmm/common/data/IVMMRecipeTypes.java
+++
@@ -1,9 +0,0 @@
-package com.ymnaytka.ivmm.common.data;
-
-import static com.gregtechceu.gtceu.common.data.GTRecipeTypes.*;
-import static com.lowdragmc.lowdraglib.gui.texture.ProgressTexture.FillDirection.*;
-
-public class IVMMRecipeTypes {
-
-    public static void init() {}
-}
--- src/main/java/com/ymnaytka/ivmm/common/data/machines/IVMMMachiensMultiA.java
+++
@@ -1,8 +0,0 @@
-package com.ymnaytka.ivmm.common.data.machines;
-
-import com.gregtechceu.gtceu.common.machine.multiblock.primitive.*;
-
-public class IVMMMachiensMultiA {
-
-    public static void init() {}
-}
